c5b91cce76430ee7fb77b694a88339ff79eed132
daniel_gzsun <daniel_gzsun@vipabc.com>
2020-05-14 17:30:03 +0800

    linklist algorithm

--- linklist/singleLink.go
+++ linklist/singleLink.go
@@ -85,6 +85,15 @@ func (s *SingleLinkList) FindByIndex(index uint) *ListNode {
 	return cur
 }
 
+func (s *SingleLinkList) GenerateLoopList(index uint) {
+	node := s.FindByIndex(index)
+	cur := s.head
+	for nil != cur.next {
+		cur = cur.next
+	}
+	cur.next = node
+}
+
 func (s *SingleLinkList) DeleteNode(node *ListNode) bool {
 	if node == nil || s.head == node {
 		return false
@@ -135,6 +144,90 @@ func (s *SingleLinkList) Reverse() bool {
 	return true
 }
 
+func (s *SingleLinkList) HasLoop() bool {
+	if s.head == nil || s.head.next == nil {
+		return false
+	}
+
+	pFast := s.head.next
+	pSlow := s.head.next
+
+	for pFast.next != nil {
+		pFast = pFast.next.next
+		if pFast == nil {
+			break
+		}
+		pSlow = pSlow.next
+		if pSlow == nil {
+			break
+		}
+		if pSlow == pFast {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *SingleLinkList) DeleteReverseNode(index uint) bool {
+	if index > s.len {
+		return false
+	}
+
+	tmpIndex := s.len - index - 1
+	cur := s.head.next
+	var i uint = 0
+	for nil != cur {
+		if i == tmpIndex {
+			tmp := cur.next
+			if tmp == nil {
+				return false
+			}
+			cur.next = tmp.next
+			tmp.next = nil
+		}
+		cur = cur.next
+		i++
+	}
+	//node := s.FindByIndex(tmpIndex)
+	//s.DeleteNode(node)
+	return true
+}
+
+func (s *SingleLinkList) DeleteReverseNode1(n uint) bool {
+	pFirst := s.head.next
+	pSecond := s.head.next
+	var index uint = 0
+	for pFirst != nil {
+		pFirst = pFirst.next
+		index++
+		if index > n+1 {
+			pSecond = pSecond.next
+		}
+	}
+	if index < n {
+		return false
+	}
+	tmp := pSecond.next
+	pSecond.next = tmp.next
+	tmp.next = nil
+	return true
+}
+
+func (s *SingleLinkList) FindMiddleNode() *ListNode {
+	index := (s.len + 1) / 2
+	return s.FindByIndex(index)
+}
+
+func (s *SingleLinkList) FindMiddleNode1() *ListNode {
+	pFast := s.head.next
+	pSlow := s.head.next
+	for nil != pFast && pFast.next != nil {
+		pFast = pFast.next.next
+		pSlow = pSlow.next
+	}
+	return pSlow
+}
+
 func Reverse2(head *ListNode) *ListNode {
 	if head == nil || head.next == nil {
 		return head
@@ -144,4 +237,54 @@ func Reverse2(head *ListNode) *ListNode {
 	head.next = nil
 	return newHead
 
+}
+
+func MergeTwoLinkList(l1 *SingleLinkList, l2 *SingleLinkList) *SingleLinkList {
+	if l1.head == nil || l1.head.next == nil {
+		return l2
+	}
+	if l2.head == nil || l2.head.next == nil {
+		return l1
+	}
+	cur1 := l1.head.next
+	cur2 := l2.head.next
+	result := NewSingleLink()
+	n := result.head
+	for cur1 != nil && cur2 != nil {
+		if cur1.value.(int) <= cur2.value.(int) {
+			n.next = cur1
+			cur1 = cur1.next
+		} else {
+			n.next = cur2
+			cur2 = cur2.next
+		}
+		result.len++
+		n = n.next
+	}
+	if cur1 == nil {
+		n.next = cur2
+	}
+
+	if cur2 == nil {
+		n.next = cur1
+	}
+	return result
+}
+
+func MergeTwoLinkList2(n1 *ListNode, n2 *ListNode) *ListNode {
+	if n1 == nil {
+		return n2
+	}
+
+	if n2 == nil {
+		return n1
+	}
+
+	if n1.value.(int) <= n2.value.(int) {
+		n1.next = MergeTwoLinkList2(n1.next, n2)
+		return n1
+	} else {
+		n2.next = MergeTwoLinkList2(n1, n2.next)
+		return n2
+	}
 }
--- linklist/singleLink_test.go
+++ linklist/singleLink_test.go
@@ -1,6 +1,7 @@
 package linklist
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -37,4 +38,94 @@ func TestReverse2(t *testing.T) {
 	}
 	list.head.next = Reverse2(list.head.next)
 	list.Print()
+}
+
+func TestSingleLinkList_HasLoop(t *testing.T) {
+	list := NewSingleLink()
+	for i := 0; i < 20; i++ {
+		list.InsertToTail(i)
+	}
+	list.Print()
+	//list.GenerateLoopList(10)
+	//	list.Print()
+	isLoop := list.HasLoop()
+	if isLoop {
+		fmt.Println("has loop in link list")
+	} else {
+		fmt.Println("no loop in link list")
+	}
+}
+
+func TestMergeTwoLinkList(t *testing.T) {
+	list1 := NewSingleLink()
+	list1.InsertToTail(1)
+	list1.InsertToTail(4)
+	list1.InsertToTail(9)
+	list1.Print()
+
+	list2 := NewSingleLink()
+	for i := 0; i < 10; i++ {
+		list2.InsertToTail(i)
+	}
+	list2.Print()
+
+	list := MergeTwoLinkList(list1, list2)
+	list.Print()
+
+}
+
+func TestMergeTwoLinkList2(t *testing.T) {
+	list1 := NewSingleLink()
+	list1.InsertToTail(1)
+	list1.InsertToTail(4)
+	list1.InsertToTail(9)
+	list1.Print()
+
+	list2 := NewSingleLink()
+	for i := 0; i < 10; i++ {
+		list2.InsertToTail(i)
+	}
+	list2.Print()
+
+	list := NewSingleLink()
+	list.head.next = MergeTwoLinkList2(list1.head.next, list2.head.next)
+	list.Print()
+}
+
+func TestSingleLinkList_DeleteReverseNode(t *testing.T) {
+	list := NewSingleLink()
+	for i := 0; i < 20; i++ {
+		list.InsertToTail(i)
+	}
+	list.Print()
+	list.DeleteReverseNode(7)
+	list.Print()
+}
+
+func TestSingleLinkList_DeleteReverseNode1(t *testing.T) {
+	list := NewSingleLink()
+	for i := 0; i < 20; i++ {
+		list.InsertToTail(i)
+	}
+	list.Print()
+	list.DeleteReverseNode1(7)
+	list.Print()
+}
+
+func TestSingleLinkList_FindMiddleNode(t *testing.T) {
+	list := NewSingleLink()
+	for i := 0; i < 20; i++ {
+		list.InsertToTail(i)
+	}
+	list.Print()
+	fmt.Println(list.FindMiddleNode().GetValue().(int))
+}
+
+func TestSingleLinkList_FindMiddleNode1(t *testing.T) {
+	list := NewSingleLink()
+	for i := 0; i < 20; i++ {
+		list.InsertToTail(i)
+	}
+	list.Print()
+	fmt.Println(list.FindMiddleNode1().GetValue().(int))
 }
